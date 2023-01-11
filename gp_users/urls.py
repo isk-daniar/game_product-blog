cbb0683eb6c31d96a79bd0afe458749861541422
Isk-Daniar <109099588+Isk-Daniar@users.noreply.github.com>
2023-01-11 16:09:00 +0500

    registration, login, logout

--- gp_users/urls.py
+++ gp_users/urls.py
@@ -4,9 +4,15 @@ from .views import *
 
 urlpatterns = [
     path('api/v1/gp-auth/', include('rest_framework.urls')),
-    path('api/v1/gp/', GPAPIList.as_view()),
-    path('api/v1/gp/<int:pk>/', GPAPIUpdate.as_view()),
-    path('api/v1/gpdelete/<int:pk>', GPAPIDestaroy.as_view()),
+    # class ExpandPost
+    path('api/v1/ep/', ExpandPostAPIList.as_view()),
+    path('api/v1/ep/<int:pk>/', ExpandPostAPIUpdate.as_view()),
+    path('api/v1/epdelete/<int:pk>', ExpandPostAPIDestaroy.as_view()),
+    # class BlogPost
+    path('api/v1/bp/', BlogPostAPIList.as_view()),
+    path('api/v1/bp/<int:pk>/', BlogPostAPIUpdate.as_view()),
+    path('api/v1/bpdelete/<int:pk>', BlogPostAPIDestaroy.as_view()),
+
     path('api/v1/auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
     path('api/v1/register/', RegisterView.as_view(), name='auth_register'),
