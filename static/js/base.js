f3aed79771cbd736ea0cdb1492fa01e12e139273
Isk-Daniar <109099588+Isk-Daniar@users.noreply.github.com>
2023-01-17 11:28:21 +0500

    drop-button

--- static/js/base.js
+++ static/js/base.js
@@ -36,3 +36,111 @@ $(window).scroll(function() {
         $(this).css('background-position', 'center ' + position)
     })
 })
+
+(function($, window) {
+    $.fn.center = function() {
+
+        return this.each(function() {
+            // Store the jQuery object for future reference
+            var element = $(this)
+
+            var parent = element.data('parent')
+
+            function center() {
+                element.css('position', 'absolute')
+                element.css('top', Math.max(0, (($(parent).height() - $(element).outerHeight()) / 2) +
+                        $(parent).scrollTop()) + 'px')
+                element.css('left', Math.max(0, (($(parent).width() - $(element).outerWidth()) / 2) +
+                        $(parent).scrollLeft()) + 'px')
+            }
+
+            center()
+
+            $(window).resize(function() {
+                center()
+            })
+
+            return element
+        })
+
+    }
+
+    // outside the scope of the jQuery plugin to
+    // keep track of all dropdowns
+    var $allDropdowns = $()
+
+    // if instantlyCloseOthers is true, then it will instantly
+    // shut other nav items when a new one is hovered over
+    $.fn.dropdownHover = function(options) {
+
+        // the element we really care about
+        // is the dropdown-toggle's parent
+        $allDropdowns = $allDropdowns.add(this.parent())
+
+        return this.each(function() {
+            var $this = $(this),
+                $parent = $this.parent(),
+                defaults = {
+                    delay: 500,
+                    instantlyCloseOthers: true,
+                },
+                data = {
+                    delay: $(this).data('delay'),
+                    instantlyCloseOthers: $(this).data('close-others'),
+                },
+                settings = $.extend(true, {}, defaults, options, data),
+                timeout
+
+            $parent.hover(function(event) {
+
+                // so a neighbor can't open the dropdown
+                if (!$parent.hasClass('open') && !$this.is(event.target)) {
+                    return true
+                }
+
+                if (settings.instantlyCloseOthers === true) {
+                    $allDropdowns.removeClass('open')
+                }
+
+                window.clearTimeout(timeout)
+                $parent.addClass('open')
+            }, function() {
+                timeout = window.setTimeout(function() {
+                    $parent.removeClass('open')
+                }, settings.delay)
+            })
+
+            // this helps with button groups!
+            $this.hover(function() {
+                if (settings.instantlyCloseOthers === true) {
+                    $allDropdowns.removeClass('open')
+                }
+
+                window.clearTimeout(timeout)
+                $parent.addClass('open')
+            })
+
+            // handle submenus
+            $parent.find('.dropdown-submenu').each(function() {
+                var $this = $(this)
+                var subTimeout
+
+                $this.hover(function() {
+
+                    window.clearTimeout(subTimeout)
+                    $this.children('.dropdown-menu').show()
+                    // always close submenu siblings instantly
+
+                    $this.siblings().children('.dropdown-menu').hide()
+                }, function() {
+                    var $submenu = $this.children('.dropdown-menu')
+                    subTimeout = window.setTimeout(function() {
+
+                        $submenu.hide()
+                    }, settings.delay)
+                })
+            })
+        })
+    }
+
+})(jQuery, window, document)
