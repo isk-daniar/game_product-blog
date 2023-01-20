20ac2cde0fedf4c0478ea86a8e4676af029a87ea
Isk-Daniar <109099588+Isk-Daniar@users.noreply.github.com>
2023-01-20 16:00:13 +0500

    blogpost-display

--- gp_users/urls.py
+++ gp_users/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('api/v1/ep/<int:pk>/', ExpandPostAPIUpdate.as_view()),
     path('api/v1/epdelete/<int:pk>', ExpandPostAPIDestaroy.as_view()),
     # class BlogPost
-    path('api/v1/bp/', BlogPostAPIList.as_view(template_name="Blog/blogpost_edited/blogpost_create.html"), name='blogpost_create'),
+    path('api/v1/bp/', BlogPostAPIList.as_view(), name='blogpost_create'),
     path('api/v1/bp/<int:pk>/', BlogPostAPIUpdate.as_view()),
     path('api/v1/bpdelete/<int:pk>', BlogPostAPIDestaroy.as_view()),
 
