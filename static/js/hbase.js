eda502b217ab10c4196d4702baf2b02215c16abd
Isk-Daniar <109099588+Isk-Daniar@users.noreply.github.com>
2023-02-08 11:49:39 +0500

    title animation

--- static/js/hbase.js
+++ static/js/hbase.js
@@ -95,3 +95,22 @@ $(window).scroll(function() {
         $(this).css('background-position', 'center ' + position)
     })
 })
+
+let typed = new Typed('#typed', { // Тут id того блока, в которм будет анимация
+    stringsElement: '#typed-strings', // Тут id блока из которого берем строки для анимации
+    typeSpeed: 100, // Скорость печати
+    startDelay: 500, // Задержка перед стартом анимации
+    backSpeed: 50, // Скорость удаления
+    loop: false // Указываем, повторять ли анимацию
+});
+$(".element").typed({
+    typeSpeed: 0, // Скорость печати
+    backSpeed: 0, // Скорость удаления
+    startDelay: 0, // Задержка перед стартом анимации
+    backDelay: 500, // Пауза перед удалением
+    loop: false, // Повтор (true или false)
+    loopCount: 2, // Число повторов, false = бесконечно
+    showCursor: true, // Отображение курсора
+    attr: null, // Атрибут
+    callback: function(){ } // Функция вызываемая после окончания работы плагина
+});
