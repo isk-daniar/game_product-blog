749e401c34122ed3d5d218d171ef4a12753e08f2
Isk-Daniar <109099588+Isk-Daniar@users.noreply.github.com>
2023-02-17 12:23:22 +0500

    title text, ep update style

--- forms.py
+++ forms.py
@@ -19,6 +19,10 @@ class AddPostBlogForm(forms.ModelForm):
 
 
 class AddExpandPostForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['postblog'].empty_label = "No blog selected"
+        self.fields['name'].empty_label = "No blog selected"
 
     class Meta:
         model = ExpandPost
